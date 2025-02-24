3475963873a76bc1a2fc4f36f1e42ff760fa80b4
Michaela Ptáčková <98548929+michaellkaa@users.noreply.github.com>
2025-02-24 23:01:44 +0100

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="p-6">
+      <router-view></router-view>
+    </div>
+  </template>
+  
---
+++ resources/js/pages/Main.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-center text-3xl font-bold text-blue-600">
+      lets go ig
+    </div>
+  </template>
+  
